85a3ac082b86de60711a9f3e927cd983390870c8
James Graham <J.Graham@software.ac.uk>
2020-02-19 13:15:53 +0000

    feat(activities): Add activity types and mediums
    resolves #8 #11 #12

--- activities/admin.py
+++ activities/admin.py
@@ -7,6 +7,16 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.ActivityType)
+class ActivityTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ActivityMedium)
+class ActivityMediumAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(models.ActivitySeries)
 class ActivitySeriesAdmin(admin.ModelAdmin):
     pass
--- activities/models.py
+++ activities/models.py
@@ -1,17 +1,56 @@
 from django.db import models
 
 
+class ActivityType(models.Model):
+    """
+    Representation of the type of activity being conducted.
+
+    Examples may include: 'research activity' or 'stakeholder engagement'.
+    """
+    #: Name of this type of activity
+    name = models.CharField(max_length=255,
+                            unique=True,
+                            blank=False, null=False)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+
+class ActivityMedium(models.Model):
+    """
+    Representation of the medium via which the activity is conducted.
+
+    Examples may include: 'face to face' or 'virtual'.
+    """
+    #: Name of this medium
+    name = models.CharField(max_length=255,
+                            unique=True,
+                            blank=False, null=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class ActivitySeries(models.Model):
     """
     A series of related :class:`Activity`s
     """
-
     class Meta:
         verbose_name_plural = 'activity series'
 
     #: Name of activity series
     name = models.CharField(max_length=1023,
                             blank=False, null=False)
+
+    #: What type of activity does this series represent?
+    type = models.ForeignKey(ActivityType,
+                             on_delete=models.PROTECT,
+                             blank=False, null=False)
+
+    #: How are activities in this series typically conducted?
+    medium = models.ForeignKey(ActivityMedium,
+                               on_delete=models.PROTECT,
+                               blank=False, null=False)
 
     def __str__(self) -> str:
         return self.name
@@ -21,7 +60,6 @@ class Activity(models.Model):
     """
     An instance of an activity - e.g. a workshop
     """
-
     class Meta:
         verbose_name_plural = 'activities'
 
@@ -33,6 +71,16 @@ class Activity(models.Model):
     series = models.ForeignKey(ActivitySeries, related_name='instances',
                                on_delete=models.PROTECT,
                                blank=True, null=True)
+
+    #: What type of activity does this series represent?
+    type = models.ForeignKey(ActivityType,
+                             on_delete=models.PROTECT,
+                             blank=False, null=False)
+
+    #: How are activities in this series typically conducted?
+    medium = models.ForeignKey(ActivityMedium,
+                               on_delete=models.PROTECT,
+                               blank=False, null=False)
 
     def __str__(self) -> str:
         return self.name
