57c29bf01d4db1d327e6e8d78e92f3dfc80e6c2e
James Graham <J.Graham@software.ac.uk>
2020-04-16 14:07:14 +0100

    refactor: Begin import of customisation app

--- breccia_mapper/views.py
+++ breccia_mapper/views.py
@@ -4,8 +4,10 @@ Views belonging to the core of the project.
 These views don't represent any of the models in the apps.
 """
 
+from django.conf import settings
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    # Template set in Django settings file - may be customised by a customisation app
+    template_name = settings.TEMPLATE_NAME_INDEX
