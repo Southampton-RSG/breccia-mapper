7289774dc73cca291472f06d05f3afd571a0435d
Matthew Grove <me@mgrove.uk>
2023-01-07 22:23:26 +0000

    [DOCS] Remove redundant extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,18 +16,8 @@ release = '1.1'
 
 extensions = [
 	'sphinx.ext.duration',
-	'sphinx.ext.doctest',
-	'sphinx.ext.autodoc',
-	'sphinx.ext.autosummary',
-	'sphinx.ext.intersphinx',
 	'myst_parser',
 ]
-
-intersphinx_mapping = {
-	'python': ('https://docs.python.org/3/', None),
-	'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
 
 myst_enable_extensions = ['colon_fence']
 
@@ -41,7 +31,3 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-# -- Options for EPUB output -------------------------------------------------
-
-epub_show_urls = 'footnote'
